188b8510ecc7c03393afb421e558c983f176c4f7
night-scholar <yexingshsuheng@aliyun.com>
2021-12-26 10:50:40 +0800

    寻找两个正序数组的中位数

--- examples/4_find_median_sorted_arrays.rs
+++ examples/4_find_median_sorted_arrays.rs
@@ -1,3 +1,4 @@
+
 impl Solution {
     pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
         let mut nums = nums1;
@@ -5,7 +6,7 @@ impl Solution {
         nums.append(&mut nums2);
         nums.sort_unstable();
         match nums.len()%2{
-            //不能用f64除以int类型
+            //注意：不能用f64除以int类型
             0=>{return (nums[nums.len()/2-1]+nums[nums.len()/2]) as f64/2.},
             1=>{return nums[nums.len()/2] as f64},
             _=>{return 0.}
